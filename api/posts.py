92b2298078aa50b918ce19f7801ad40ff7e90e66
Thanh2311 <Tcn1995@my.yorku.ca>
2022-08-02 23:52:59 -0400

    Update patch route
    adds error handling for parameters

--- api/posts.py
+++ api/posts.py
@@ -11,7 +11,6 @@ from middlewares import auth_required
 SORT_BY = ["id", "reads", "likes", "popularity"]
 DIRECTION = ["asc", "desc"]
 DELIMITER = ","
-UPDATEBLE = ["text", "tags"]
 
 @api.post("/posts")
 @auth_required
@@ -86,7 +85,7 @@ def update(postId):
     if user is None:
         return abort(401)
 
-    # get post if exists
+    # validate if post exists
     post = Post.query.get(postId)
     if post is None:
         return jsonify({"error": "Post not found"}), 404
@@ -98,20 +97,33 @@ def update(postId):
     if not any([x.user_id == user.id for x in user_posts]):
         return jsonify({"error": "User is not author of this post"}), 403
 
-    # update post with valid json data
+    # get updated properties
     data = request.get_json()
-    for key in UPDATEBLE:
-        if data.get(key, None) is not None:
-            setattr(post, key, data[key])
+    author_ids = data.get("authorIds", None)
+    tags = data.get("tags", None)
+    text = data.get("text", None)
+
+    # check for invalid parameters
+    if text is not None and not isinstance(text, str):
+        return jsonify({"error": "text must be a string"}), 400
+    if tags is not None and not (isinstance(tags, list) and all(isinstance(tag, str) for tag in tags)):
+        return jsonify({"error": "tags must be an array of strings"}), 400
+    if author_ids is not None and not (isinstance(author_ids, list) and all(isinstance(id, int) for id in author_ids)):
+        return jsonify({"error": "authorIds must be an array of author ids"}), 400
+
+    # update post with valid json data
+    if tags is not None:
+        post.tags = tags
+    if text is not None:
+        post.text = text
 
     # check if authors changed
-    author_ids = data.get("authorIds", None)
     if author_ids is not None:
         # delete old and add new authors
         UserPost.query.filter(UserPost.post_id == postId).delete()
         for author_id in author_ids:
             user_post = UserPost(user_id=author_id, post_id=post.id)
-            user_posts.append(user_post)
+            db.session.add(user_post)
     else:
         # create array of existing author ids
         author_ids = []
